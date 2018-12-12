7eba3f9512ca948140d104d66b16346d57744942
莲示 <lianshi.whb@alibaba-inc.com>
2018-12-12 16:43:21 +0800

    add != == && defer and reg mook

--- calculator.cc
+++ calculator.cc
@@ -12,7 +12,7 @@ enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-  NUMBER, TK_HEX, TK_REG, TK_NEQ, TK_AND
+  NUMBER, TK_HEX, TK_REG, TK_NEQ, TK_AND, TK_DEFER
 };
 
 Token tokens[32];
@@ -24,73 +24,51 @@ int nr_token;
 
 int op_priority(int op) {
     switch (op) {
+        case TK_AND: return 1;
+        case TK_EQ:
+        case TK_NEQ: return 2;
         case '+':
-        case '-': return 1;
+        case '-': return 3;
         case '*':
-        case '/': return 2;
+        case '/': return 4;
+        case TK_DEFER:
+        case TK_REG: return 5;
         default: return 0;
     }
 }
 
 void make_token(char *input) {
-    for (char* iter = input; *iter != '\0'; iter++) {
-        if (isdigit(*iter)) {
-            int i;
-            for (i = 0; isdigit(iter[i]); i++) {
-                tokens[nr_token].str[i] = iter[i];
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == '0' && input[i+1] == 'x') {
+            i += 2;
+            int j;
+            for (j = 0; isdigit(input[i+j]) || (input[i+j] >= 'a' && input[i+j] <= 'f'); j++) {
+                tokens[nr_token].str[j] = input[i+j];
             }
-            iter += i - 1;
+            i += j - 1;
+            tokens[nr_token++].type = TK_HEX;
+        } else if (isdigit(input[i])) {
+            int j;
+            for (j = 0; isdigit(input[i+j]); j++) {
+                tokens[nr_token].str[j] = input[i+j];
+            }
+            i += j - 1;
             tokens[nr_token++].type = NUMBER;
-        } else if (*iter != ' ') {
-            tokens[nr_token++].type = *iter;
-        }
-    }
-}
-
-unsigned expr(int* step) {
-    unsigned rst = 0;
-    int num = 0, next = 0;
-    int op = '+';
-
-    for (int i = *step; i < nr_token; i++) {
-        if (tokens[i].type == NUMBER) {
-            sscanf(tokens[i].str, "%u", &next);
-        } else if (tokens[i].type == '(') {
+        } else if (input[i] == '=' && input[i+1] == '=') {
             i++;
-            next = expr(&i);
-        }
-        switch (op) {
-            case '+': {
-                rst += num;
-                num = next;
-                break;
-            }
-            case '-': {
-                rst += num;
-                num = -next;
-                break;
-            }
-            case '*': {
-                num *= next;
-                break;
-            }
-            case '/': {
-                num /= next;
-                break;
-            }
-        }
-        
-        if (++i < nr_token) {
-            op = tokens[i].type;
-            if (op == ')') {
-                *step = i;
-                break;
-            }
+            tokens[nr_token++].type = TK_EQ;
+        } else if (input[i] == '&' && input[i+1] == '&') {
+            i++;
+            tokens[nr_token++].type = TK_AND;
+        } else if (input[i] == '!' && input[i+1] == '=') {
+            i++;
+            tokens[nr_token++].type = TK_NEQ;
+        } else if (input[i] == '$') {
+            tokens[nr_token++].type = TK_REG;
+        } else if (input[i] != ' ') {
+            tokens[nr_token++].type = input[i];
         }
     }
-
-    rst += num;
-    return rst;
 }
 
 int make_poland() {
@@ -109,6 +87,7 @@ int make_poland() {
                 }
                 break;
             }
+            case TK_HEX:
             case NUMBER: {
                 poland_output[poland_len++] = tokens[i];
                 break;
@@ -134,23 +113,37 @@ int make_poland() {
 
 int cal_poland(int poland_len) {
     int top = 0;
+    int ans = 0;
     int i;
     int val;
-    int ans;
     int top1, top2;
     for (i = 0; i < poland_len; i++) {
         if (poland_output[i].type == NUMBER) {
             sscanf(poland_output[i].str, "%d", &val);
             num_stack[top++] = val;
+        } else if (poland_output[i].type == TK_HEX) {
+            sscanf(poland_output[i].str, "%x", &val);
+            num_stack[top++] = val;
         } else {
             top1 = num_stack[--top];
-            top2 = num_stack[--top];
-            switch (poland_output[i].type) {
-                case '+': ans = top2 + top1; break;
-                case '-': ans = top2 - top1; break;
-                case '*': ans = top2 * top1; break;
-                case '/': ans = top2 / top1; break;
+            if (poland_output[i].type == TK_DEFER) {
+                ans = top1 * 2;
+            } else if (poland_output[i].type == TK_REG) {
+                ans = top1 * 3;
+            } else {
+                // binary op
+                top2 = num_stack[--top];
+                switch (poland_output[i].type) {
+                    case '+': ans = top2 + top1; break;
+                    case '-': ans = top2 - top1; break;
+                    case '*': ans = top2 * top1; break;
+                    case '/': ans = top2 / top1; break;
+                    case TK_AND: ans = top2 && top1; break;
+                    case TK_NEQ: ans = top2 != top1; break;
+                    case TK_EQ:  ans = top2 == top1; break;
+                }
             }
+            
             num_stack[top++] = ans;
         }
     }
@@ -175,17 +168,32 @@ bool check_parentheses(int begin, int end) {
     return cnt == 0;
 }
 
-int calculate(char* input) {
+unsigned calculate(char* input) {
     make_token(input);
+
+    for (int i = 0; i < nr_token; i++) {
+        if (tokens[i].type == '*') {
+            if (i == 0) {
+                tokens[i].type = TK_DEFER;
+            }
+            switch (tokens[i-1].type) {
+                case '+': case '-': case '*': case '/':
+                case TK_AND: case TK_NEQ: case TK_EQ: {
+                    tokens[i].type = TK_DEFER;
+                }
+            }
+        }
+    }
+
     return expr();
     // int step = 0;
     // return expr(&step);
 }
 
 int main() {
-    char input[] = "( 723014127+ 585011881*2016025616/ 509096286)";
-    // char input[] = "4 +3*(2- 1)";
-    // char input[] = "4 + 1 * 3";
+    // char input[] = "( 723014127+ 585011881*2016025616/ 509096286)";
+    char input[] = "4 +3*(2+ 1) == 0xd && *1 == 2";
+    // char input[] = "0xd";
     cout << calculate(input);
     return 0;
 }
