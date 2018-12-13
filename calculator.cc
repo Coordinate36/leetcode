6dcf51f12057ead9eb516bbca1c7e8b3563f59e6
莲示 <lianshi.whb@alibaba-inc.com>
2018-12-13 16:08:00 +0800

    add decimal support

--- calculator.cc
+++ calculator.cc
@@ -8,11 +8,19 @@ typedef struct {
     char str[30];
 } Token;
 
+typedef struct {
+    int type;
+    union {
+        int int_;
+        double double_;
+    };
+} Operand;
+
 enum {
   TK_NOTYPE = 256, TK_EQ,
 
   /* TODO: Add more token types */
-  NUMBER, TK_HEX, TK_REG, TK_NEQ, TK_AND, TK_DEFER
+  NUMBER, TK_HEX, TK_REG, TK_NEQ, TK_AND, TK_DEFER, TK_NEG, DECIMAL
 };
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
@@ -26,10 +34,12 @@ int grp_w[] = {0, 1, 2, 3, 4, 5, 6, 7};
 int grp_l[] = {8, 9, 10, 11, 12, 13, 14, 15};
 int grp_b[] = {20, 21, 22, 23, 24, 25, 26, 27};
 
-Token tokens[32];
-Token poland_stack[32];
-Token poland_output[32];
-int num_stack[32];
+const int TOKEN_NUM = 255;
+
+Token tokens[TOKEN_NUM];
+Token poland_stack[TOKEN_NUM];
+Token poland_output[TOKEN_NUM];
+Operand num_stack[TOKEN_NUM];
 
 int nr_token;
 
@@ -42,8 +52,8 @@ int op_priority(int op) {
         case '-': return 3;
         case '*':
         case '/': return 4;
-        case TK_DEFER:
-        case TK_REG: return 5;
+        case TK_NEG:
+        case TK_DEFER: return 5;
         default: return 0;
     }
 }
@@ -63,8 +73,16 @@ void make_token(char *input) {
             for (j = 0; isdigit(input[i+j]); j++) {
                 tokens[nr_token].str[j] = input[i+j];
             }
+            if (input[i+j] == '.') {
+                tokens[nr_token].str[j] = input[i+j];
+                for (j++; isdigit(input[i+j]); j++) {
+                    tokens[nr_token].str[j] = input[i+j];
+                }
+                tokens[nr_token++].type = DECIMAL;
+            } else {
+                tokens[nr_token++].type = NUMBER;
+            }
             i += j - 1;
-            tokens[nr_token++].type = NUMBER;
         } else if (input[i] == '=' && input[i+1] == '=') {
             i++;
             tokens[nr_token++].type = TK_EQ;
@@ -117,6 +135,8 @@ int make_poland() {
                 }
                 break;
             }
+            case DECIMAL:
+            case TK_REG:
             case TK_HEX:
             case NUMBER: {
                 poland_output[poland_len++] = tokens[i];
@@ -141,36 +161,66 @@ int make_poland() {
     return poland_len;
 }
 
-int cal_poland(int poland_len) {
+Operand cal_poland(int poland_len) {
     int top = 0;
-    int ans = 0;
+    Operand ans;
+    Operand val;
+    Operand top1, top2;
     int i;
-    int val;
-    int top1, top2;
     for (i = 0; i < poland_len; i++) {
         if (poland_output[i].type == NUMBER) {
-            sscanf(poland_output[i].str, "%d", &val);
+            val.type = NUMBER;
+            sscanf(poland_output[i].str, "%d", &val.int_);
             num_stack[top++] = val;
         } else if (poland_output[i].type == TK_HEX) {
-            sscanf(poland_output[i].str, "%x", &val);
+            val.type = NUMBER;
+            sscanf(poland_output[i].str, "%x", &val.int_);
+            num_stack[top++] = val;
+        } else if (poland_output[i].type == DECIMAL) {
+            val.type = DECIMAL;
+            sscanf(poland_output[i].str, "%lf", &val.double_);
             num_stack[top++] = val;
         } else if (poland_output[i].type == TK_REG) {
-            num_stack[top++] = reg_value(poland_output[i].str + 1);
+            val.type = NUMBER;
+            val.int_ = reg_value(poland_output[i].str + 1);
+            num_stack[top++] = val;
         } else {
             top1 = num_stack[--top];
+            ans.type = top1.type;
             if (poland_output[i].type == TK_DEFER) {
-                ans = top1 * 2;
+                ans.int_ = top1.int_ * 2;
+            } else if (poland_output[i].type == TK_NEG) {
+                if (top1.type == NUMBER) {
+                    ans.int_ = -top1.int_;
+                } else {
+                    ans.double_ = -top1.double_;
+                }
             } else {
                 // binary op
                 top2 = num_stack[--top];
-                switch (poland_output[i].type) {
-                    case '+': ans = top2 + top1; break;
-                    case '-': ans = top2 - top1; break;
-                    case '*': ans = top2 * top1; break;
-                    case '/': ans = top2 / top1; break;
-                    case TK_AND: ans = top2 && top1; break;
-                    case TK_NEQ: ans = top2 != top1; break;
-                    case TK_EQ:  ans = top2 == top1; break;
+                if (top2.type == DECIMAL) {
+                    ans.type = DECIMAL;
+                }
+                if (ans.type == NUMBER) {
+                    switch (poland_output[i].type) {
+                        case '+': ans.int_ = top2.int_ + top1.int_; break;
+                        case '-': ans.int_ = top2.int_ - top1.int_; break;
+                        case '*': ans.int_ = top2.int_ * top1.int_; break;
+                        case '/': ans.int_ = top2.int_ / top1.int_; break;
+                        case TK_AND: ans.int_ = top2.int_ && top1.int_; break;
+                        case TK_NEQ: ans.int_ = top2.int_ != top1.int_; break;
+                        case TK_EQ:  ans.int_ = top2.int_ == top1.int_; break;
+                    }
+                } else {
+                    switch (poland_output[i].type) {
+                        case '+': ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) + (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                        case '-': ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) - (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                        case '*': ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) * (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                        case '/': ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) / (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                        case TK_AND: ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) && (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                        case TK_NEQ: ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) != (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                        case TK_EQ:  ans.double_ = (top2.type == NUMBER ? top2.int_: top2.double_) == (top1.type == NUMBER ? top1.int_: top1.double_); break;
+                    }
                 }
             }
             
@@ -180,9 +230,9 @@ int cal_poland(int poland_len) {
     return num_stack[top-1];
 }
 
-unsigned expr() {
+Operand expr() {
     int poland_len = make_poland();
-    return cal_poland(poland_len);
+    return cal_poland(poland_len);   
 }
 
 bool check_parentheses(int begin, int end) {
@@ -198,7 +248,7 @@ bool check_parentheses(int begin, int end) {
     return cnt == 0;
 }
 
-unsigned calculate(char* input) {
+Operand calculate(char* input) {
     make_token(input);
 
     for (int i = 0; i < nr_token; i++) {
@@ -207,23 +257,34 @@ unsigned calculate(char* input) {
                 tokens[i].type = TK_DEFER;
             }
             switch (tokens[i-1].type) {
-                case '+': case '-': case '*': case '/':
+                case '+': case '-': case '*': case '/': case '(':
                 case TK_AND: case TK_NEQ: case TK_EQ: {
                     tokens[i].type = TK_DEFER;
+                }
+            }
+        } else if (tokens[i].type == '-') {
+            if (i == 0) {
+                tokens[i].type = TK_NEG;
+            }
+            switch (tokens[i-1].type) {
+                case '+': case '-': case '*': case '/': case '(':
+                case TK_AND: case TK_NEQ: case TK_EQ: {
+                    tokens[i].type = TK_NEG;
                 }
             }
         }
     }
 
     return expr();
-    // int step = 0;
-    // return expr(&step);
 }
 
 int main() {
     // char input[] = "( 723014127+ 585011881*2016025616/ 509096286)";
-    char input[] = "4 +3*(2+ 1) == 0xd && $ax == 0";
-    // char input[] = "$ax";
-    cout << calculate(input);
+    char input[] = "( 626516348)+ 505496690/ 826252910";
+    // char input[] = "(1.2+(3*2) +(($eax-$eax) +(*$eip-1**$eip)+(0x5--5+ *0x100005 - *0x100005))*4)";
+    Operand rst = calculate(input);
+    cout << rst.type << endl;
+    cout << rst.int_ << endl;
+    cout << (rst.type == NUMBER ? rst.int_: rst.double_) << endl;
     return 0;
 }
